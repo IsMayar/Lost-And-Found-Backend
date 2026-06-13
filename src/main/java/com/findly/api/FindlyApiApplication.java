aa581cb71e09a6615033b45cbe7547a8caaed835
Ismael Mayar <ismailmayar113@gmail.com>
2026-06-13 15:10:26 +0430

    chore: initialize Spring Boot backend project

---
+++ src/main/java/com/findly/api/FindlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.findly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindlyApiApplication.class, args);
+	}
+
+}
